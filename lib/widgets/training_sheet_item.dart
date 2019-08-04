4b30c10c3716c936b9dec34f6b01f82d3013b352
Raul de Melo <melo.raulf@gmail.com>
2019-08-04 18:46:16 +0200

    Move use when it clicks to see a training details

--- lib/widgets/training_sheet_item.dart
+++ lib/widgets/training_sheet_item.dart
@@ -1,23 +1,39 @@
 import 'package:flutter/material.dart';
+
 import '../models/training_sheet.dart';
+import '../screens/training_sheet_detail_screen.dart';
 
 class TrainingSheetItem extends StatelessWidget {
   final TrainingSheet trainingSheet;
 
   const TrainingSheetItem({Key key, this.trainingSheet}) : super(key: key);
 
+  void _selectTrainingSheet(BuildContext context) {
+    Navigator.of(context).pushNamed(
+      'training-sheet',
+      arguments: {
+        'trainingSheet': trainingSheet,
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Text(
-            trainingSheet.alias,
-            style: Theme.of(context).textTheme.title,
-          ),
-          Text(trainingSheet.muscularGroup),
-        ],
+    return InkWell(
+      onTap: () => _selectTrainingSheet(context),
+      borderRadius: BorderRadius.circular(8),
+      splashColor: Theme.of(context).primaryColorDark,
+      child: Card(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              trainingSheet.alias,
+              style: Theme.of(context).textTheme.title,
+            ),
+            Text(trainingSheet.muscularGroup),
+          ],
+        ),
       ),
     );
   }
