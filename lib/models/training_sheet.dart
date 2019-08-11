fd6db92fdc7a2278a796e6f9707c6035272ee14c
Raul de Melo <melo.raulf@gmail.com>
2019-08-11 14:21:50 +0200

    Connect App to API server instead dummy data

--- lib/models/training_sheet.dart
+++ lib/models/training_sheet.dart
@@ -1,16 +1,43 @@
 import 'package:flutter/foundation.dart';
-import 'package:my_fitness_buddy_flutter/models/exercise.dart';
+
+import '../models/exercise.dart';
 
 class TrainingSheetModel {
   final String id;
   final String alias;
   final String muscularGroup;
   final List<ExerciseModel> exercises;
+  final int restTimeMin;
+  final int restTimeMax;
 
   const TrainingSheetModel({
     @required this.id,
     @required this.alias,
     @required this.muscularGroup,
     @required this.exercises,
+    this.restTimeMin,
+    this.restTimeMax,
   });
+
+  factory TrainingSheetModel.fromJson(Map<String, dynamic> json) {
+    final List<dynamic> exercises = json['exercises'] ?? [];
+    final dynamic restTime = json['rest_time'] ?? 0;
+
+    return TrainingSheetModel(
+      alias: json['alias'],
+      id: json['id'],
+      muscularGroup: json['group_name'],
+      restTimeMin: restTime != 0 ? restTime[0] : restTime,
+      restTimeMax: restTime != 0 ? restTime[1] : restTime,
+      exercises: exercises.map((exercise) {
+        return ExerciseModel(
+          advancedTechnique: exercise['advanced_technique'],
+          id: exercise['id'],
+          name: exercise['name'],
+          repetitions: exercise['repetition'],
+          series: exercise['series'],
+        );
+      }).toList(),
+    );
+  }
 }
--- lib/screens/training_sheets_screen.dart
+++ lib/screens/training_sheets_screen.dart
@@ -1,17 +1,51 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 import '../widgets/training_sheet_item.dart';
+import '../models/training_sheet.dart';
 
-import '../dummy_data.dart';
-
-class TrainingSheetsScreen extends StatelessWidget {
+class TrainingSheetsScreen extends StatefulWidget {
   static String route = '/';
+
+  @override
+  _TrainingSheetsScreenState createState() => _TrainingSheetsScreenState();
+}
+
+class _TrainingSheetsScreenState extends State<TrainingSheetsScreen> {
+  var _isInit = false;
+  List<TrainingSheetModel> _trainingSheet = [];
+
+  @override
+  void didChangeDependencies() {
+    if (!_isInit) {
+      setState(() {
+        _isInit = true;
+      });
+
+      http
+          .get('https://my-fitness-buddy-server.herokuapp.com/routine')
+          .then((response) {
+        final data = json.decode(response.body) as Map<String, dynamic>;
+        List<TrainingSheetModel> nextList = [];
+
+        data['data'].forEach((routine) {
+          nextList.add(TrainingSheetModel.fromJson(routine));
+        });
+
+        setState(() {
+          _trainingSheet = nextList;
+        });
+      });
+    }
+    super.didChangeDependencies();
+  }
 
   @override
   Widget build(BuildContext context) {
     return GridView(
       padding: EdgeInsets.all(20),
-      children: DUMMY_TRAINING_SHEET.map((t) {
+      children: _trainingSheet.map((t) {
         return TrainingSheetItem(
           trainingSheet: t,
         );
