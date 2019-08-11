3fa37a290b9ac3e3255d715c786c05da2ed493f4
Raul de Melo <melo.raulf@gmail.com>
2019-08-11 14:16:24 +0200

    Comment out dummy data for training sheets

--- lib/dummy_data.dart
+++ lib/dummy_data.dart
@@ -4,43 +4,43 @@ import './models/exercise.dart';
 import './models/diet_sheet.dart';
 import './models/food_option.dart';
 
-const DUMMY_TRAINING_SHEET = const [
-  TrainingSheetModel(
-    alias: 'Treino A',
-    id: 't1',
-    muscularGroup: 'Peito + Costa',
-    exercises: [
-      ExerciseModel(
-        id: '_z8zh9ctuu',
-        name: 'Supino Declinado Halteres',
-        advancedTechnique: 'Rest \'n\' Pause 3x',
-        repetitions: 12,
-        series: 5,
-      ),
-      ExerciseModel(
-        id: '_b5nufyj03',
-        name: 'Supino Reto Halteres',
-        advancedTechnique: '',
-        repetitions: 10,
-        series: 5,
-      ),
-    ],
-  ),
-  TrainingSheetModel(
-    alias: 'Treino B',
-    id: 't2',
-    muscularGroup: 'Costa + Trapézio',
-    exercises: [
-      ExerciseModel(
-        id: '_fez0il7kg',
-        name: 'Levantamento Terra',
-        advancedTechnique: '',
-        repetitions: 8,
-        series: 5,
-      ),
-    ],
-  ),
-];
+// const DUMMY_TRAINING_SHEET = const [
+//   TrainingSheetModel(
+//     alias: 'Treino A',
+//     id: 't1',
+//     muscularGroup: 'Peito + Costa',
+//     exercises: [
+//       ExerciseModel(
+//         id: '_z8zh9ctuu',
+//         name: 'Supino Declinado Halteres',
+//         advancedTechnique: 'Rest \'n\' Pause 3x',
+//         repetitions: 12,
+//         series: 5,
+//       ),
+//       ExerciseModel(
+//         id: '_b5nufyj03',
+//         name: 'Supino Reto Halteres',
+//         advancedTechnique: '',
+//         repetitions: 10,
+//         series: 5,
+//       ),
+//     ],
+//   ),
+//   TrainingSheetModel(
+//     alias: 'Treino B',
+//     id: 't2',
+//     muscularGroup: 'Costa + Trapézio',
+//     exercises: [
+//       ExerciseModel(
+//         id: '_fez0il7kg',
+//         name: 'Levantamento Terra',
+//         advancedTechnique: '',
+//         repetitions: 8,
+//         series: 5,
+//       ),
+//     ],
+//   ),
+// ];
 
 const DUMMY_DIET_SHEET = const [
   DietSheetModel(
