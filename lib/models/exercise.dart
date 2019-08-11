9f01ebbeefccfc6a2d9c0739c7c1d3cd33074992
Raul de Melo <melo.raulf@gmail.com>
2019-08-11 14:16:58 +0200

    Change data type and remove prop requirement

--- lib/models/exercise.dart
+++ lib/models/exercise.dart
@@ -3,15 +3,15 @@ import 'package:flutter/foundation.dart';
 class ExerciseModel {
   final String id;
   final String name;
-  final int series;
-  final int repetitions;
   final String advancedTechnique;
+  final String series;
+  final String repetitions;
 
   const ExerciseModel({
     @required this.id,
     @required this.name,
     @required this.series,
     @required this.repetitions,
-    @required this.advancedTechnique,
+    this.advancedTechnique = '',
   });
 }
